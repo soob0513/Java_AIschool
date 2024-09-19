56efc4465fb86b7089637110b7302870c2a44d95
최수빈 <165890322+soob0513@users.noreply.github.com>
2024-09-19 17:07:33 +0900

    0919

--- Ex0913/MyJDBC.sql
+++ Ex0913/MyJDBC.sql
@@ -1,2 +1,2 @@
-create table member values (id varchar2(100), pw varchar2(100), nick varchar2(100));
+create table member (id varchar2(100), pw varchar2(100), nick varchar2(100));
 select * from member;
